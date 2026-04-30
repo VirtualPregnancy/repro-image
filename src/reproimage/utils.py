7c9e655e6e2ba7d0b2ed4347cfdc06db67b79444
Brock Sherlock <brock.sherlock@auckland.ac.nz>
2026-04-30 17:59:25 +1200

    Added supplementary outputs to mean_wave

--- src/reproimage/utils.py
+++ src/reproimage/utils.py
@@ -128,6 +128,7 @@ def mean_wave(x_values, y_values, verbose=False):
 
     # Convert the list of interpolated waves to a NumPy array for calculations
     interpolated_waves_np = np.vstack(interpolated_waves)
+    total_beats = interpolated_waves_np.shape[0]
 
     # Calculate the initial average and standard deviation
     average_wave = np.mean(interpolated_waves_np, axis=0)
@@ -186,7 +187,16 @@ def mean_wave(x_values, y_values, verbose=False):
         plt.grid(True)
         plt.show()
 
-    return new_average_wave, x_common
+    num_filtered_waves = filtered_waves_np.shape[0]
+    num_excluded_waves = len(excluded_waves)
+
+    diagnostics = {
+        "total_beats": total_beats,
+        "num_beats_retained": num_filtered_waves,
+        "num_beats_excluded": num_excluded_waves
+    }
+
+    return new_average_wave, x_common, diagnostics
 
 
 class MetaData:
